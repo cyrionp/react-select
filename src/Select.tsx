c1c2f052795f8e358018b0cfb3d9f25be1da757c
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2023-07-18 17:46:32 +0300

    created: single selection

---
+++ src/Select.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import styles from "./Select.module.css"
+
+type SelectOption = {
+	label: string;
+	value: any;
+};
+
+type SelectProps = {
+	options: SelectOption[];
+	value?: SelectOption;
+	onChange: (value: SelectOption | undefined) => void;
+};
+
+export function Select({ value, onChange, options }: SelectProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function clearOptions(){
+        onChange(undefined);
+    }
+
+    function selectOption(option:SelectOption){
+        onChange(option)
+    }
+
+    return (
+        <div
+            onBlur={()=>setIsOpen(false)}
+            onClick={()=>setIsOpen(prev=>!prev)}
+            tabIndex={0} className={styles.container}
+        >
+            <span className={styles.value}>{value?.label}</span>
+            <button onClick={e=>{
+                e.stopPropagation()
+                clearOptions()
+            }} className={styles["clear-btn"]}>&times;</button>
+            <div className={styles.divider}></div>
+            <div className={styles.caret}></div>
+            <ul className={`${styles.options} ${isOpen ? styles.show: ""}`}>
+                {options.map(option=>(
+                    <li onClick={e=>{
+                        e.stopPropagation()
+                        selectOption(option)
+                        setIsOpen(false)
+                    }} key={option.label} className={styles.option}>
+                        {option.label}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
